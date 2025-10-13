4b17d06e11c815b08e5665c385166a1a1e13398e
Nadirr <aznabaefff@yandex.ru>
2025-10-13 19:17:46 +0500

    Сделана основа 1 части. Реализованы модели Role and Person.

---
+++ Laba_4_Aznabaev_Nadir_BPI_23_01/Model/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_4_Aznabaev_Nadir_BPI_23_01.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public Person() { }
+
+        public Person(int id, int roleId, string firstName, string lastName, DateTime birthday)
+        {
+            Id = id;
+            RoleId = roleId;
+            FirstName = firstName;
+            LastName = lastName;
+            Birthday = birthday;
+        }
+    }
+
+}
